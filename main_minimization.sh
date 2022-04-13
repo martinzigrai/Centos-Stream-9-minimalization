9ea1e41d99fea432bb9fe313fd41c7ad09819b9f
Martin Žigrai <martin.zigrai@gmail.com>
2022-04-13 10:40:52 +0000

    Pridanie komentárov k jednotlivým častiam

--- main_minimization.sh
+++ main_minimization.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+#zmazanie logovacích súborov, súborov medzipamäti a obnovenie databáze rpm
 find /var/log -name "*.log*" -delete
 rm -rfv /var/cache/*
 rpm --rebuilddb
 
+#zmazanie manuálových stránok, informácií a dokumentácie k programom
 rm -rfv /user/share/doc/
 rm -rfv /user/share/man/
 rm -rfv /usr/share/info/
 
+#rozparsovanie výstupu príkazu locale
 system_lang=`locale | cut -f2 -d= | cut -f1 -d. | head -n 1`
 
+#odstránenie nepoužitých lokalizácií
 for target in `find /usr/share/locale -maxdepth 1 -not -name $system_lang*`; do
 	rm -fv $target/LC_MESSAGES/*
 done
 
+#vytvorenie funkcie remove
 function remove {
 	for target in "$@"; do
 		rm -fv $target
@@ -23,8 +28,10 @@ function remove {
 DIR1=$HOME/all_modules.txt
 DIR2=$HOME/current_modules.txt
 
+#hladanie dostupných modulov jadra
 find /lib/modules/$(uname -r) -type f -name `*.ko*` > $DIR1
 
+#hladanie cesty k modulom jadra
 for module in `lsmod | cut -f1 -d" " | tail -n +2`; do
 	filename=`modinfo $module -n`
 	echo "$filename" >> $DIR2
@@ -33,6 +40,8 @@ done
 sort $DIR1 | uniq > file1.sorted
 sort $DIR2 | uniq > file2.sorted
 
+#odstránenie nepoužitých modulov pomocou funkcie remove
 remove `comm -23 file1.sorted file2.sorted`
 
+#odstránenie nepoužívaného firmware
 remove `find /usr/lib/firmware -atime +2`
